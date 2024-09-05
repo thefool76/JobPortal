9b40fee51a5604fac93c4b755535e3cdf02b5896
thefool76 <sumitmishra1135@gmail.com>
2024-09-05 18:28:17 +0530

    added

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    bg: {
+        type: String,
+        default: "bg-grey-100"
+    }
+})
+</script>
+
+<template>
+    <div :class="`${bg} p-6 rounded-lg shadow-md`">
+        <slot></slot>
+    </div>
+</template>
